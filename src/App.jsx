dfd838b450e268bfadeeced8eeed266e8ea92b88
nyniraula <nyniraula@gmail.com>
2026-05-31 14:05:35 +0545

    Scaffold Project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className=" text-7xl">abcdefghi</h1>
+      <h1 className="font-sans text-7xl">abcdefghi</h1>
+      <h1 className="font-syne text-7xl">Test</h1>
+    </div>
+  );
+};
+
+export default App;
