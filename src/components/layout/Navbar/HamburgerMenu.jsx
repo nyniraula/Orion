9fb8a3272cf277139c2949305d320660a71c15e7
nyniraula <nyniraula@gmail.com>
2026-06-01 12:13:28 +0545

    FIX: minor semantic bugs

--- src/components/layout/Navbar/HamburgerMenu.jsx
+++ src/components/layout/Navbar/HamburgerMenu.jsx
@@ -1,20 +1,27 @@
+import { ChevronRight } from "lucide-react";
+
 const HamburgerMenu = ({ linkItems, setIsHamMenuOpen }) => {
   return (
-    <button
-      className="w-full bg-white px-8 pb-8 pt-4 flex flex-col gap-2 md:hidden  animate-slide-down"
+    <div
+      className="w-full bg-white px-6 pb-8 pt-4 flex flex-col gap-2  md:hidden  animate-slide-down"
       onClick={() => {
         setIsHamMenuOpen(false);
       }}
     >
       {linkItems.map((el, idx) => {
         return (
-          <div className=" py-3 flex items-center justify-between" key={idx}>
+          <button
+            className=" py-3 px-2 flex items-center justify-between hover:bg-[#f6f6f6]"
+            key={idx}
+          >
             <span>{el.title}</span>
-            <span>{">"}</span>
-          </div>
+            <span>
+              <ChevronRight strokeWidth={0.8} size={20} />
+            </span>
+          </button>
         );
       })}
-    </button>
+    </div>
   );
 };
 
--- src/components/layout/Navbar/SearchDropdown.jsx
+++ src/components/layout/Navbar/SearchDropdown.jsx
@@ -2,7 +2,7 @@ import { Search, X } from "lucide-react";
 
 const SearchDropdown = ({ setIsSearchBoxOpen }) => {
   return (
-    <div className="bg-white w-full px-4 pb-6 md:px-8 md:pb-8  pt-4 flex items-center gap-2 md:gap-4">
+    <div className="bg-white w-full px-4 pb-6 md:px-8 md:pb-8  pt-4 flex items-center gap-2 md:gap-4 animate-slide-down">
       <div className="flex w-full">
         <button className="px-4 py-3 md:py-4 bg-[#f6f6f6] ">
           <Search strokeWidth={0.8} size={20} color="gray" />
